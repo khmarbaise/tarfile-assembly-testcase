46182a8f3ac01f805b1441bbc6168ce0d9e2b609
Alessandro Mecca <amecca@qmetric.co.uk>
2014-03-11 18:06:20 +0000

    Added stacktrace

--- extract_tar.py
+++ extract_tar.py
@@ -1,22 +1,25 @@
 import tarfile
 import os
+import sys
+import traceback
 
 filename = 'target/tarfile-assembly-testcase-1.0-SNAPSHOT-src.tar.gz'
 f = open(filename)
 
-print("Trying to open", filename)
+print "Trying to open", filename
 
 if not os.path.exists(filename):
-    print("File", filename, "does not exists. Did you run 'mvn package'?")
+    print "File", filename, "does not exists. Did you run 'mvn package'?"
     exit(1)
 
 if not os.path.isfile(filename):
-    print("File", filename, "is not a file.")
+    print "File", filename, "is not a file."
     exit(1)
 
 try:
     tarfile.open(fileobj=f, mode='r:gz')
 except Exception, e:
-    print("Failed with exception", e.message)
+    type_, value_, traceback_ = sys.exc_info()
+    print(''.join(traceback.format_exception(type_, value_, traceback_)))
 
-print('Try to unpack the file from command-line with:\n   tar xvfz ' + filename)
+print('Now try to unpack the file from command-line with:\n   tar xvfz ' + filename)
